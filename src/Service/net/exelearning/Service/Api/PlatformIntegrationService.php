c71a9fdcb3eb939da6452e255ab03c85568afec9
Pablo Amaya <pabloamaya@educarex.es>
2025-10-08 13:24:40 +0200

    Fix: Improve error handling in platformPetitionSet method to include user-friendly error message

--- src/Service/net/exelearning/Service/Api/PlatformIntegrationService.php
+++ src/Service/net/exelearning/Service/Api/PlatformIntegrationService.php
@@ -14,6 +14,7 @@ use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Throwable;
 
 class PlatformIntegrationService implements PlatformIntegrationServiceInterface
 {
@@ -55,6 +56,7 @@ class PlatformIntegrationService implements PlatformIntegrationServiceInterface
         } catch (Exception $e) {
             /* TODO: send error message */
             $this->logger->error('Exception:'.$e->getMessage());
+            $content['error'] = 'There was a problem with the upload';
             /* Stop the application */
         }
 
@@ -83,7 +85,7 @@ class PlatformIntegrationService implements PlatformIntegrationServiceInterface
             ]);
 
             $content = $response->toArray();
-        } catch (\Exception $e) {
+        } catch (Throwable $e) {
             $this->logger->error('platform upload error:'.$e->getMessage(), ['className' => get_class($e), 'file:' => $this, 'line' => __LINE__]);
             $content['error'] = 'There was a problem with the upload';
         }
