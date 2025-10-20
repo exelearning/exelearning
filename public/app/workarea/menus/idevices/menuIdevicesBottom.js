a74fdaeea65b897908da53829af3b340f0114d5f
jmtomas <jmtomas@tresipunt.com>
2025-10-20 14:59:00 +0200

    fixes for revision iii

--- public/app/workarea/menus/idevices/menuIdevicesBottom.js
+++ public/app/workarea/menus/idevices/menuIdevicesBottom.js
@@ -103,6 +103,11 @@ export default class MenuIdevicesBottom {
         let settingIcon = document.createElement('div');
         settingIcon.classList.add('idevice_icon', 'settings-icon');
         settingIcon.id = 'setting-menuIdevices';
+        settingIcon.setAttribute('title');
+        settingIcon.setAttribute('data-bs-title', ideviceData.title);
+        settingIcon.setAttribute('data-bs-placement', 'top');
+        settingIcon.setAttribute('data-bs-toggle', 'tooltip');
+        window.bootstrap.Tooltip.getOrCreateInstance(settingIcon);
         return settingIcon;
     }
 
