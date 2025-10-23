0f9e39b3fe4de960fc4d8a3c1cd831b66e542172
ignaciogros <ignacio@gros.es>
2025-10-23 12:13:56 +0200

    New "Minify iDevice" option.

--- public/app/workarea/project/idevices/content/ideviceNode.js
+++ public/app/workarea/project/idevices/content/ideviceNode.js
@@ -301,6 +301,14 @@ export default class IdeviceNode {
                     // In some cases the idevice will not be able to be edited
                     blockButtonEditClass = '';
                 }
+                // Set the Minify iDevice icon
+                let minifyIdeviceIcon = 'chevron-down-icon-green';
+                const iDevice = $(
+                    "div.idevice_body[idevice-id='" + this.odeIdeviceId + "']"
+                );
+                if (iDevice.is(':hidden')) {
+                    minifyIdeviceIcon = 'chevron-up-icon-green';
+                }
                 blockButtonsHTML = `
                 <div class="dropdown exe-actions-menu">
                     <button class="btn-action-menu btn button-secondary secondary-green button-narrow button-combo combo-left d-flex justify-content-center align-items-center btn-move-up-idevice" type="button" id=moveUpIdevice${id} title="${_('Move up')}"><span class="small-icon arrow-up-icon-green"</span></button>
@@ -313,7 +321,8 @@ export default class IdeviceNode {
                         <li><button class="dropdown-item button-action-block" id="cloneIdevice${id}"><span class="small-icon duplicate-icon-green"></span>${_('Clone')}</button></li>
                         <li><button class="dropdown-item button-action-block" id="moveIdevice${id}"><span class="small-icon move-icon-green"></span>${_('Move to')}</button></li>
                         <li class="exe-advanced"><button class="dropdown-item button-action-block" id="exportIdevice${id}"><span class="small-icon download-icon-green"></span>${_('Export')}</span></button></li>
-                     </ul>
+                    </ul>
+                    <button class="btn-action-menu btn button-secondary secondary-green button-narrow button-combo combo-left d-flex justify-content-center align-items-center btn-minify-idevice" type="button" id=minifyIdevice${id} title="${_('Toggle content')}"><span id="minifyIdevice${id}icon" class="small-icon ${minifyIdeviceIcon}"</span></button>
                 </div>`;
                 // Check links (disabled) <li><button class="dropdown-item button-action-block" id="checkLinksIdevice${id}"><span class="auto-icon" aria-hidden="true">links</span>${_('Check links')}</button></li>
                 this.ideviceButtons.innerHTML = blockButtonsHTML;
@@ -328,6 +337,7 @@ export default class IdeviceNode {
                 this.addBehaviouCloneIdeviceButton();
                 this.addBehaviourMoveToPageIdeviceButton();
                 this.addBehaviourExportIdeviceButton();
+                this.addBehaviourMinifyIdeviceButton();
                 this.addNoTranslateForGoogle();
                 // Check links (disabled) this.addBehaviouCheckBrokenLinksIdeviceButton();
                 break;
@@ -1182,6 +1192,34 @@ export default class IdeviceNode {
                             );
                         }
                     });
+            });
+    }
+
+    /**
+     *
+     */
+    addBehaviourMinifyIdeviceButton() {
+        this.ideviceButtons
+            .querySelector('#minifyIdevice' + this.odeIdeviceId)
+            .addEventListener('click', (element) => {
+                if (eXeLearning.app.project.checkOpenIdevice()) return;
+                const iDevice = $(
+                    "div.idevice_body[idevice-id='" + this.odeIdeviceId + "']"
+                );
+                if (iDevice.length != 1) return false;
+                const icn = this.ideviceButtons.querySelector(
+                    '#minifyIdevice' + this.odeIdeviceId + 'icon'
+                );
+                if (icn.classList.contains('chevron-down-icon-green')) {
+                    icn.classList.remove('chevron-down-icon-green');
+                    icn.classList.add('chevron-up-icon-green');
+                    iDevice.hide();
+                } else {
+                    icn.classList.remove('chevron-up-icon-green');
+                    icn.classList.add('chevron-down-icon-green');
+                    iDevice.show();
+                }
+                return false;
             });
     }
 
