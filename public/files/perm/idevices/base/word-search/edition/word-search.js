adc7c105ed0f8afbab23c17302bddc6b4ef5e9bd
mnarvaezm <mnarvaez007@gmail.com>
2025-11-04 00:53:10 +0100

    Update getTabIA() parameter from 0 to 9 to match other iDevices configuration

--- public/files/perm/idevices/base/word-search/edition/word-search.js
+++ public/files/perm/idevices/base/word-search/edition/word-search.js
@@ -511,7 +511,7 @@ var $exeDevice = {
             ${$exeDevicesEdition.iDevice.gamification.scorm.getTab()}
             ${$exeDevicesEdition.iDevice.gamification.common.getLanguageTab(this.ci18n)}
             ${$exeDevicesEdition.iDevice.gamification.share.getTab(true, 0, true)}
-            ${$exeDevicesEdition.iDevice.gamification.share.getTabIA(0)}
+            ${$exeDevicesEdition.iDevice.gamification.share.getTabIA(9)}
         </div>
         `;
         this.ideviceBody.innerHTML = html;
