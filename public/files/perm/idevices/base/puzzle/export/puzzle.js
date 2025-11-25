b537fa4f462871db2a521dfdcf6d5415bb77310b
mnarvaezm <mnarvaez007@gmail.com>
2025-11-25 16:28:59 +0100

    make fix

--- public/files/perm/idevices/base/puzzle/export/puzzle.js
+++ public/files/perm/idevices/base/puzzle/export/puzzle.js
@@ -723,7 +723,7 @@ var $eXePuzzle = {
                     height: newHeight + 'px',
                 });
             });
-        
+
         $eXePuzzle.adjustImageDivHeight(instance);
     },
 
@@ -731,19 +731,19 @@ var $eXePuzzle = {
         const $imageDiv = $('#pzlImageDiv-' + instance);
         const $imagePuzzle = $('#pzlImagePuzzle-' + instance);
         const $image = $('#pzlImage-' + instance);
-        
+
         if ($imagePuzzle.length && $image.length) {
             const puzzleWidth = $imagePuzzle.width();
             const puzzleHeight = $imagePuzzle.height();
-            
+
             if (puzzleWidth > 0 && puzzleHeight > 0) {
                 const containerWidth = $imageDiv.width();
                 const aspectRatio = puzzleHeight / puzzleWidth;
                 const calculatedHeight = containerWidth * aspectRatio;
-                
+
                 $imageDiv.css({
-                    'height': calculatedHeight + 'px',
-                    'padding-top': '0'
+                    height: calculatedHeight + 'px',
+                    'padding-top': '0',
                 });
             }
         }
