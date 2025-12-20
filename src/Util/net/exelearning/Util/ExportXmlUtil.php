b5200fb916cd96522a72f02fe7a66496e35cff94
ignaciogros <ignacio@gros.es>
2025-12-20 17:25:10 +0100

    Fix #786 - Missing "Teacher mode" toggler in untitled packages.

--- src/Util/net/exelearning/Util/ExportXmlUtil.php
+++ src/Util/net/exelearning/Util/ExportXmlUtil.php
@@ -2018,9 +2018,16 @@ class ExportXmlUtil
         // Package title and subtitle container
         $packageHeaderDiv = null;
         $packageTitleValue = isset($odeProperties['pp_title']) ? $odeProperties['pp_title']->getValue() : '';
+
+        // The single page export has its own package title
         if (Constants::EXPORT_TYPE_HTML5_SP == $exportType) {
             $packageTitleValue = '';
-        } // The single page export has its own package title
+        } elseif ('' == $packageTitleValue) {
+            // There should be no untitled packages
+            // Use a NO-BREAK SPACE in those cases
+            $packageTitleValue = '&#160;';
+        }
+
         if ('' != $packageTitleValue || (Constants::EXPORT_TYPE_HTML5_SP != $exportType && '' != $subtitle)) {
             $packageHeaderDiv = $pageHeader->addChild('div');
             $packageHeaderDiv->addAttribute('class', 'package-header');
